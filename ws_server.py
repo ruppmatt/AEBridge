c9a7194230e2ddf6290274ca2429289d874b2a33
Matthew R. Rupp <ruppmatt@gmail.com>
2016-09-22 15:28:00 -0400

    Prototype working for TX webworker messages.

--- ws_server.py
+++ ws_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, Namespace
 import json
 
@@ -17,55 +17,54 @@ avida_messages = []
 def messages():
     return render_template('messages.html')
 
-class AvidaClient(Namespace):
-    def on_connect(msg):
-        avida_client = request.sid;
 
-    def on_disconnect(msg):
+class AvidaClient(Namespace):
+    def on_connect(self):
+        global avida_client
+        avida_client = request.sid
+        print('Avida client connected:', avida_client)
+
+    def on_disconnect(self):
+        global avida_client
+        print('Avida client disconnected:', avida_client)
         avida_client = None
 
-    def on_message(msg):
-        avida_messages.append(msg);
+    def on_ui_msg(self, msg):
+        global message_socket
+        print('ui_msg: ', str(msg))
+        if messages_socket:
+            emit('ui_msg', msg, namespace='/messages', room=messages_socket);
+
+    def on_av_msg(self, msg):
+        global message_socket
+        print ('av_msg: ', str(msg))
+        if messages_socket:
+            emit('av_msg', msg, namespace='/messages', room=messages_socket);
 
 
 class MessagesClient(Namespace):
     def on_connect(msg):
+        global messages_socket
         messages_socket = request.sid
+        print('Message client connected:', messages_socket)
 
     def on_disconnect(msg):
+        global messages_socket
+        print('Message client disconnected:', messages_socket)
         messages_socket = None
-
-    def on_receive_filtered(msg):
-        if (console_socket):
-            emit('receive_filtered', room=console_socket)
 
     def on_refresh_messages(msg):
         emit('messages', json.dumps(avida_messages))
 
     def on_send_command(msg):
+        global avida_socket
         if avida_socket:
             emit('command', msg, room=avida_socket)
 
-
-class ConsoleClient(Namespace):
-    def on_connect(msg):
-        console_socket = request.sid
-
-    def on_disconnect(msg):
-        console_socket = None
-
-    def on_get_filtered(msg):
-        if messages_socket:
-            emit('send_filtered', room=messages_socket)
-
-    def on_send_command(msg):
-        if avida_socket:
-            emit('command', msg, room=avida_socket)
 
 
 socketio.on_namespace(AvidaClient('/avida'))
 socketio.on_namespace(MessagesClient('/messages'))
-socketio.on_namespace(ConsoleClient('/console'))
 
 if __name__ == '__main__':
     socketio.run(app)
