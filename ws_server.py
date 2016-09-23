a905a19112735098077dd9b2b8d693e3ea1df283
Matthew R. Rupp <ruppmatt@gmail.com>
2016-09-23 17:41:27 -0400

    Messages format better.  Redis incrementing in place.

--- ws_server.py
+++ ws_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, Namespace
 import json
+import redis
 
 
 app = Flask(__name__)
@@ -10,7 +11,25 @@ avida_socket = None
 messages_socket = None
 console_socket = None
 
-avida_messages = []
+r_server = redis.Redis('localhost');
+r_server.set('_ndx', -1);
+
+
+def BuildMessage(j):
+    global r_server
+    msg = StripMessage(j)
+    msg['_ndx'] = r_server.incr('_ndx')
+    return msg
+
+
+def StripMessage(j):
+    rv = {k:j[k] for k in ['type','name','level'] if k in j}
+    if rv['type'] == 'response':
+        if 'name' not in j['request']:
+            rv['name'] = '(' + j['request']['type'] + ')'
+        else:
+            rv['name'] = '(' + j['request']['type'] + '@' + j['request']['name'] + ')'
+    return rv
 
 
 @app.route('/messages')
@@ -30,16 +49,14 @@ class AvidaClient(Namespace):
         avida_client = None
 
     def on_ui_msg(self, msg):
-        global message_socket
-        print('ui_msg: ', str(msg))
+        global messages_socket
         if messages_socket:
-            emit('ui_msg', msg, namespace='/messages', room=messages_socket);
+            emit('ui_msg', BuildMessage(msg), namespace='/messages', room=messages_socket);
 
     def on_av_msg(self, msg):
-        global message_socket
-        print ('av_msg: ', str(msg))
+        global messages_socket
         if messages_socket:
-            emit('av_msg', msg, namespace='/messages', room=messages_socket);
+            emit('av_msg', BuildMessage(msg), namespace='/messages', room=messages_socket);
 
 
 class MessagesClient(Namespace):
@@ -53,13 +70,10 @@ class MessagesClient(Namespace):
         print('Message client disconnected:', messages_socket)
         messages_socket = None
 
-    def on_refresh_messages(msg):
-        emit('messages', json.dumps(avida_messages))
-
     def on_send_command(msg):
         global avida_socket
         if avida_socket:
-            emit('command', msg, room=avida_socket)
+            emit('command', msg, namespace='/avida', room=avida_socket)
 
 
 
